358d49076f94867f95801c9b231ff1bba6b493dd
amitanand765 <amitanand.765@gmail.com>
2026-05-16 10:57:02 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,8 +1,18 @@
-const CACHE = 'marketcast-v1';
-const ASSETS = ['/', '/index.html', '/manifest.json'];
+const CACHE = 'marketcast-v2';
 
 self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll([
+      '/marketcast-app/',
+      '/marketcast-app/index.html',
+      '/marketcast-app/manifest.json',
+    ]))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(clients.claim());
 });
 
 self.addEventListener('fetch', e => {
